ade8a2acad8e546d71bfa4442e2def3e0bf91e7f
Guillermo <Guillermolopezargudo@gmail.com>
2025-04-09 09:58:00 +0200

    cambios back

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ from config import Config
 from peewee import DoesNotExist
 from datetime import date
 from peewee import fn
+from dotenv import load_dotenv
 import hashlib
 import os
 import base64
 
 app = Flask(__name__)
 create_tables()
+load_dotenv()
 app.config['UPLOAD_FOLDER_IMAGES'] = 'static/images'
 app.config['UPLOAD_FOLDER_VIDEOS'] = 'static/videos'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'mov', 'avi'}
