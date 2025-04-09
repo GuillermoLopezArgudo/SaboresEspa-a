de62b23fdd36e6b035dc191dd08d35c01a4578b8
Guillermo <Guillermolopezargudo@gmail.com>
2025-04-09 11:50:56 +0200

    v5

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'db')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'user')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '1234')
     MYSQL_DB = os.getenv('MYSQL_DB', 'recipesSpain')
